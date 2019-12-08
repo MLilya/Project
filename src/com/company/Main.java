14c6cedaca2be586101471cc696811dc8a740e76
Lilya <lilya.122@yandex.ru>
2019-12-08 17:47:24 +0300

    локальные переменные

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("start main");
-
+        int b = 3;
         some();
         System.out.println("end main");
 
@@ -13,11 +13,13 @@ public class Main {
 
     public static void some() {
         System.out.println("  start some");
+        int b = 2;
         some2();
         System.out.println("  end some");
     }
 
     public static void some2() {
+        int b = 1;
         System.out.println("  start some2");
         System.out.println("  end some2");
     }
