41e3bfce06d7566f042bfbef5dde62f429794700
Lilya <lilya.122@yandex.ru>
2019-12-08 17:29:34 +0300

    стек вызовов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Привет");
-        System.out.println("Пока");
-        System.out.println("lilya");
-        System.out.println("renat");
-        int a = 5;
-        System.out.println(a);
-        System.out.println(a * a);
+        System.out.println("start main");
+
+        some();
+        System.out.println("end main");
+
+
+    }
+
+    public static void some() {
+        System.out.println("  start some");
+        some2();
+        System.out.println("  end some");
+    }
+
+    public static void some2() {
+        System.out.println("  start some2");
+        System.out.println("  end some2");
     }
 }
