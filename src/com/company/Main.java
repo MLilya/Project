a5ab41488e7d9403aed47c4a41bd73c5204e3c70
Lilya <lilya.122@yandex.ru>
2019-12-08 18:35:35 +0300

    постинкремент, прединкремент

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int some = 5;
         int c = 1;
-        String a = "lorem";
-//        String b = "ipsum";
-//        System.out.println(a + b);
-        System.out.println(c + some + a + c + some);
+        System.out.println(c++);
+        System.out.println(++c);
+        System.out.println(c);
     }
 
 }
