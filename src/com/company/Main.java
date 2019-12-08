0b70edc56913200152c47835a80f100b16fc4bb8
Lilya <lilya.122@yandex.ru>
2019-12-08 18:30:05 +0300

    конкатенация  операция склеивания объектов линейной структуры, обычно строк.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,24 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("start main");
-        int b = 3;
-        some();
-        System.out.println("end main");
-
-
+        int some = 5;
+        int c = 1;
+        String a = "lorem";
+//        String b = "ipsum";
+//        System.out.println(a + b);
+        System.out.println(c + some + a + c + some);
     }
 
-    public static void some() {
-        System.out.println("  start some");
-        int b = 2;
-        some2();
-        System.out.println("  end some");
-    }
-
-    public static void some2() {
-        int b = 1;
-        System.out.println("  start some2");
-        System.out.println("  end some2");
-    }
 }
+
