ba11bf4c6e79fb3636d5b371a74007c9afedaa17
Lilya <lilya.122@yandex.ru>
2019-12-15 02:37:07 +0300

    класс

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int c = 1;
-        System.out.println(c++);
-        System.out.println(++c);
-        System.out.println(c);
-//        hello
+        Cat cat1 = new Cat();
+        Cat cat2 = cat1;
+
+
 
     }
 
