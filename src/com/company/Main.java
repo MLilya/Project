d9e37f3aba645a7398840503c096a66c82741046
Lilya <lilya.122@yandex.ru>
2019-12-15 03:02:21 +0300

    зачем тут мейн

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
 public class Main {
+    Cat cat1 = new Cat();
+    Cat cat2;
 
     public static void main(String[] args) {
-        Cat cat1 = new Cat();
-        Cat cat2 = cat1;
-
+        Main main = new Main();
+        int b = 0;
 
 
     }
